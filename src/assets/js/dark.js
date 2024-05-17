aebb725ed0c7b570ab9c7201b77f4b1d57694f51
Dylan Hoag <dysco712@gmail.com>
2024-05-16 22:09:32 -0600

    test

--- src/assets/js/dark.js
+++ src/assets/js/dark.js
@@ -8,45 +8,34 @@ document.addEventListener('DOMContentLoaded', () => {
     let currentTheme = localStorage.getItem('theme') || 'light';
     document.documentElement.setAttribute('data-theme', currentTheme);
     
-    // Show/hide icons based on current theme
-    if (currentTheme === 'light') {
-        moonIcon.style.display = 'none';
-        sunIcon.style.display = 'block';
-        // Change check icon src to check-dark.svg
-        checkIcons.forEach(icon => {
-            icon.src = '/assets/svgs/check-dark.svg';
-        });
-    } else {
-        moonIcon.style.display = 'block';
-        sunIcon.style.display = 'none';
-        checkIcons.forEach(icon => {
-            icon.src = '/assets/svgs/check.svg';
-        });
-    }
-    
+    // Function to update icon visibility and check icon sources
+    const updateIcons = (theme) => {
+        if (theme === 'light') {
+            moonIcon.style.display = 'none';
+            sunIcon.style.display = 'block';
+            checkIcons.forEach(icon => {
+                icon.src = '/assets/svgs/check-dark.svg';
+            });
+        } else {
+            moonIcon.style.display = 'block';
+            sunIcon.style.display = 'none';
+            checkIcons.forEach(icon => {
+                icon.src = '/assets/svgs/check.svg';
+            });
+        }
+    };
+
+    // Initialize the icons based on the current theme
+    updateIcons(currentTheme);
+
     // Add event listener to the toggle button
     if (toggle) {
         toggle.addEventListener('click', () => {
             let theme = document.documentElement.getAttribute('data-theme');
-            if (theme === 'light') {
-                theme = 'dark';
-                moonIcon.style.display = 'block';
-                sunIcon.style.display = 'none';
-                // Change check icon src to check.svg
-                checkIcons.forEach(icon => {
-                    icon.src = '/assets/svgs/check.svg';
-                });
-            } else {
-                theme = 'light';
-                moonIcon.style.display = 'none';
-                sunIcon.style.display = 'block';
-                // Change check icon src to check-dark.svg
-                checkIcons.forEach(icon => {
-                    icon.src = '/assets/svgs/check-dark.svg';
-                });
-            }
+            theme = (theme === 'light') ? 'dark' : 'light';
             document.documentElement.setAttribute('data-theme', theme);
             localStorage.setItem('theme', theme);
+            updateIcons(theme);
         });
     }
 });
